b8c944646151a9aefc4ba27d18db77ebf3c03dd5
Manoj Gupta <manoj.gupta10296@gmail.com>
2019-06-26 10:45:51 +0530

    Updates

---
+++ precache-manifest.e8b72797049d01c177f142c055fd416f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c4230aec7b38328e7b015b7878d410f",
+    "url": "/manoj10296/frontendtask/index.html"
+  },
+  {
+    "revision": "9f81be6c8c3e9376a268",
+    "url": "/manoj10296/frontendtask/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "bd9bd8a95bbbdb3f37b9",
+    "url": "/manoj10296/frontendtask/static/css/main.81c962e9.chunk.css"
+  },
+  {
+    "revision": "9f81be6c8c3e9376a268",
+    "url": "/manoj10296/frontendtask/static/js/2.716789b9.chunk.js"
+  },
+  {
+    "revision": "bd9bd8a95bbbdb3f37b9",
+    "url": "/manoj10296/frontendtask/static/js/main.c36f769c.chunk.js"
+  },
+  {
+    "revision": "3e7e861cf5c2f395f46b",
+    "url": "/manoj10296/frontendtask/static/js/runtime~main.71b0d800.js"
+  }
+]);
